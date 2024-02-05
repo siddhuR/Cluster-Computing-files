209087aba12407ae8f7f74fa3662035d5222e8b0
Routhu Siddhartha <92938829+siddhuR@users.noreply.github.com>
2024-02-05 19:20:09 +0530

    Add files via upload

---
+++ ScalaFiles/Arrays/lazyList.scala
@@ -0,0 +1,12 @@
+// import scala.collection.immutable.LazyList
+
+object lazyList {
+  def main(args: Array[String]){
+    //val lazyList = 20 #:: 30 #:: 40 #:: LazyList.empty    // head #:: tail #:: LazyList.empty
+    //lazyList.foreach(println)
+    
+    //val lazyList = 1 #:: 2 #:: 3 #:: LazyList.empty
+    
+    
+  }
+}
