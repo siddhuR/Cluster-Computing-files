c27c2f9173829c4b799c4b280a37aafe20b50c49
Routhu Siddhartha <92938829+siddhuR@users.noreply.github.com>
2024-05-08 18:07:03 +0530

    Add files via upload

--- ScalaFiles/Arrays/lazyList.scala
+++ ScalaFiles/Arrays/lazyList.scala
@@ -2,9 +2,9 @@
 
 object lazyList {
   def main(args: Array[String]){
-    //val lazyList = 20 #:: 30 #:: 40 #:: LazyList.empty    // head #:: tail #:: LazyList.empty
-    //lazyList.foreach(println)
-    
+//    val lazyList = 20 #:: 30 #:: 40 #:: LazyList.empty    // head #:: tail #:: LazyList.empty
+//    lazyList.foreach(println)
+//    
     //val lazyList = 1 #:: 2 #:: 3 #:: LazyList.empty
     
     
